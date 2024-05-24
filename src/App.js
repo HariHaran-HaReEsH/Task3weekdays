320ae6b8e9c88e657c86d7d17d8cc4d6671e376f
Hariharan <hariharantlr2401@gmail.com>
2024-05-24 10:30:07 +0530

    firstcmmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  let [inputValue, setInputValue] = React.useState('');
+  const [message,setmessage]= React.useState('');
+  const getevent=(event)=>{
+    setInputValue(event.target.value);
+    
+  }
+  function check(){
+    switch(inputValue){
+    case '1':
+      setmessage('This is Sunday');  
+      break;
+    case '2':
+      setmessage('This is Monday');
+      break;
+    case '3':
+      setmessage('This is Tuesday');
+      break;
+    case '4':
+      setmessage('This is Wednesday');
+      break;
+    case '5':
+      setmessage('This is Thursday');
+      break;
+    case '6':
+      setmessage('This is Friday');
+      break;
+    case '7':
+      setmessage('This is Saturday');
+      break;
+    default:
+      setmessage('Entered Wrong Day');
+    
+ }
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <input type='number' value={inputValue} onChange={getevent}></input>
+    <button onClick={check} >Check</button>
+    <p>{message}</p>
     </div>
   );
 }
-
+ 
 export default App;
